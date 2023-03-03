dea286550712757e77749874ce965bfddbf1bb07
Prince Saharan <prince.saharan2580@gmail.com>
2023-03-03 21:49:04 +0530

    MINOR GLITCH

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,24 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
 
-    // const submitted=() => {
-
-    //     setTimeout(() => {
-
-
-    //         <div className="alert alert-success" role="alert">
-    //             This is a success alert—check it out!
-    //         </div>
-
-    //     }, 1500);
-    // }
+    
+    const {data,setData} = useState("")
     return (
         <>
             <div className="card">
                 <div className="card-block">
                     <h2>Contact</h2>
-                    <form action="https://formspree.io/f/xjvdraeo" method="POST" className="reveal-content">
+                    <form action="https://formspree.io/f/xjvdraeo" method="POST"  className="reveal-content">
                         <div className="form-group">
                             <input autoComplete="off" required  type="email" id="email" className="form-control" name="Email" placeholder="Email"></input>
                         </div>
@@ -29,10 +20,10 @@ const Contact = () => {
                             <input autoComplete="off" required type="text" id="subject" className="form-control" name="Subject" placeholder="Subject"></input>
                         </div>
                         <div className="form-group">
-                            <textarea autoComplete="off" required className="form-control" id="message"  name="Message" rows="5" placeholder="Enter your message"></textarea>
+                            <textarea autoComplete="off" value={data} onChange={(e)=>setData(e.target.value)} required className="form-control" id="message"  name="Message" rows="5" placeholder="Enter your message"></textarea>
                         </div>
                         <div className="form-group">
-                            <button  type="submit" className=" btn btn-primary">Send message</button>
+                            <button   type="submit" className=" btn btn-primary">Send message</button>
                         </div>
                     </form>
                 </div>
